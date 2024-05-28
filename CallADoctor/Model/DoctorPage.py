3687633dd1d1971db7c27b3123f4be85bcf95484
calvin050101 <calvinchin01@gmail.com>
2024-05-28 14:30:44 +0800

    Code for getting all patients

--- CallADoctor/Model/DoctorPage.py
+++ CallADoctor/Model/DoctorPage.py
@@ -4,12 +4,22 @@ from tkinter import messagebox
 from tkinter.font import BOLD, Font
 from PIL import Image, ImageTk
 import os
-
+from firebase_admin import credentials, initialize_app, db
 dir = os.path.dirname(__file__)
+
+serviceAccountKeyFile = os.path.join(dir, '../calladoctor-serviceAccountKey.json')
 
 class DoctorPage(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent, bg="#F6F6E9")
+        cred = credentials.Certificate(serviceAccountKeyFile)
+        initialize_app(cred, {'databaseURL': 'https://calladoctor-5001-default-rtdb.asia-southeast1.firebasedatabase.app/'})
+
+        # Get a reference to the clinics node in the database
+        patients_ref = db.reference('patients')
+
+        # Retrieve the clinic data
+        patients = patients_ref.get()
 
         # navigation bar
         nav_bar = tk.Frame(bg="#6B9778")
@@ -31,10 +41,6 @@ class DoctorPage(tk.Frame):
         submit_button = tk.Button(self, text="Search", bg="#0275DD", fg="#ffffff")
         submit_button.grid(row=3, column=1, pady=(10, 0), sticky="w")
 
-        patients = [
-            {"name": "Stephen Lee", "email": "stephen@gmail.com", "phoneNum": "00123542122"}, 
-            {"name": "Aaron Lee", "email": "aaron@gmail.com", "phoneNum": "221543542"},
-        ]
         self.listPatients(patients)
 
         # Ensure the patient_frame expands correctly
@@ -43,24 +49,21 @@ class DoctorPage(tk.Frame):
         self.grid_columnconfigure(1, weight=0)
 
     def listPatients(self, patients):
-         for row, patient in enumerate(patients, 4):
+         for row, (_, patient) in enumerate(patients.items(), 4):
             # Create a frame with a white background to display patient details
             patient_frame = tk.Frame(self, bg='white', width=400, height=30)
-            patient_frame.grid(row=row, column=0, padx=20, pady=20, sticky="n")
+            patient_frame.grid(row=row, column=0, padx=20, pady=20, sticky="w")
             self.grid_rowconfigure(row, weight=1)
 
-            name_label = tk.Label(patient_frame, text=f"Patient Name: {patient['name']}", bg="white")
+            name_label = tk.Label(patient_frame, text=f"Patient Name: {patient.get('username')}", 
+                                  bg="white")
             name_label.grid(row=0, column=0, padx=20, sticky="w")
 
             view_button = tk.Button(patient_frame, text="View", bg="#0275DD", fg="#ffffff")
             view_button.grid(row=0, column=1, padx=10, pady=5, sticky="w")
 
-            email_label = tk.Label(patient_frame, text=f"Email: {patient['email']}", bg="white")
+            email_label = tk.Label(patient_frame, text=f"Email: {patient.get('email')}", bg="white")
             email_label.grid(row=1, column=0, padx=20, sticky="w")
-
-            phone_num_label = tk.Label(patient_frame, text=f"Phone Number: {patient['phoneNum']}", 
-                                       bg="white")
-            phone_num_label.grid(row=2, column=0, padx=20, sticky="w")
 
     # def search_patient(self):
     #     # Function to handle the search functionality
